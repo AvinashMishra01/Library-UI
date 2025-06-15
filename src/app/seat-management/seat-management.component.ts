735f8ab9947a5a7f927fd0b4c7646fbc3119dd4d
Avinash-Mishra <avinashmishralu@gmail.com>
2025-06-15 21:52:04 +0530

    Applied stashed changes

---
+++ src/app/seat-management/seat-management.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seat-management',
+  imports: [],
+  templateUrl: './seat-management.component.html',
+  styleUrl: './seat-management.component.css'
+})
+export class SeatManagementComponent {
+
+}
