628abef6d70fdfcb253b7100da8393832e0123b2
Avinash-Mishra <avinashmishralu@gmail.com>
2025-06-17 22:56:19 +0530

    dashboard working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, computed, HostListener, input, output, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './layout/footer/footer.component';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
+import { MainComponent } from './layout/main/main.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FooterComponent,SidebarComponent,MainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Library-Management';
+
+  isLeftSidebarCollapsed = signal<boolean>(false);
+  screenWidth = signal<number>(window.innerWidth);
+
+  @HostListener('window:resize')
+  onResize() {
+    this.screenWidth.set(window.innerWidth);
+    if (this.screenWidth() < 768) {
+      this.isLeftSidebarCollapsed.set(true);
+    }
+  }
+
+  ngOnInit(): void {
+    this.isLeftSidebarCollapsed.set(this.screenWidth() < 768);
+  }
+
+  changeIsLeftSidebarCollapsed(isLeftSidebarCollapsed: boolean): void {
+    this.isLeftSidebarCollapsed.set(isLeftSidebarCollapsed);
+  }
 }
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,5 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { ChartOptions } from 'chart.js';
 import { ChartConfiguration } from 'chart.js';
 import { BaseChartDirective } from 'ng2-charts';
 
@@ -15,18 +14,7 @@ import { BaseChartDirective } from 'ng2-charts';
 })
 export class DashboardComponent {
 
-
-
-  bookings = [
-  { room: 'Room 101', date: 'Today', duration: '2h' },
-  { room: 'Room 202', date: '04/24/2024', duration: '2h' },
-  { room: 'Room 105', date: '04/23/2024', duration: '2h' },
-  { room: 'Room 105', date: '04/22/2024', duration: '2h' },
-  { room: 'Room 303', date: '04/22/2024', duration: '2h' }
-];
-
-
-revenueChartData: ChartConfiguration<'line'>['data'] = {
+ revenueChartData: ChartConfiguration<'line'>['data'] = {
     labels: ['Mon','Tue','Wed','Thu','Fri','Sat','Sun'],
     datasets: [{ data: [200,300,250,280,400,500,620], label:'Revenue' }]
   };
@@ -38,5 +26,4 @@ revenueChartData: ChartConfiguration<'line'>['data'] = {
     labels: ['Slot A','Slot B','Slot C'],
     datasets: [{ data: [40,35,25] }]
   };
-
 }
